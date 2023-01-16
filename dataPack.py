3df050a73b2bfd9e2b8d7469f34957c234c90600
1i9h7_b1u3 <1012796366@qq.com>
2023-01-16 13:55:36 +0800

    数据文件

---
+++ dataPack.py
@@ -0,0 +1,10 @@
+class driverData:
+    def __init__(self):
+        self.dstFrame = None
+        self.srcFrame = None
+        self.isNodding = False
+        self.isBlinking = False
+        self.isYawning = False
+        self.isFreeing = False
+        self.isSleeping = False
+        self.timeStamp = None
